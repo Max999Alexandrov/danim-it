937ad29198dbe3fc927ef4fcbc1fc840a96242de
Max Alexandrov <154638042+Max999Alexandrov@users.noreply.github.com>
2023-12-23 00:08:07 +0200

    Create giro.dart

---
+++ giro.dart
@@ -0,0 +1,25 @@
+var MOCK_RECIPE = Recipe(
+    cookbookId: MOCK_COOKBOOK_ID,
+    id: MOCK_RECIPE_ID,
+    nodeVersion: MOCK_NODE_VERSION,
+    name: "LOUD's Wooden sword lv1 buy recipe",
+    description: 'this recipe is used to buy wooden sword lv1.',
+    costPerBlock: Coin(denom: 'upylon', amount: '1000000'),
+    version: MOCK_VERSION,
+    coinInputs: [],
+    itemInputs: [],
+    entries: EntriesList(coinOutputs: [], itemOutputs: [
+      ItemOutput(
+        id: 'copper_sword_lv1',
+        doubles: [],
+        longs: [],
+        strings: [],
+        mutableStrings: [],
+        transferFee: [],
+        tradePercentage: DecString.decStringFromDouble(0.1),
+        tradeable: true,
+      ),
+    ], itemModifyOutputs: []),
+    outputs: [
+      WeightedOutputs(entryIds: ['copper_sword_lv1'], weight: Int64(1))
+    ],
