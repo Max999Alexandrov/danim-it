4abdd29a639e8da3e29afedb3aff2a8c98f15ff3
Max Alexandrov <154638042+Max999Alexandrov@users.noreply.github.com>
2023-12-23 00:05:32 +0200

    Update dng avi.sh

--- dng avi.sh
+++ dng avi.sh
@@ -25,3 +25,13 @@ WILL_INSTALL=0
     # Print out block and transaction hash from which to sync state.
 echo "trust_height: $BLOCK_HEIGHT"
 echo "trust_hash: $TRUST_HASH"
+
+
+# install pylonsd if not exist
+if [ $WILL_INSTALL -eq 0 ];
+then 
+    command -v pylonsd > /dev/null 2>&1 || { echo >&1 "installing pylonsd"; make install; }
+else
+    echo >&1 "installing pylonsd"
+    rm -rf $HOME/.pylons*
+    go install ./...
